88d93cbc36ad30ac9859328f932ccd9387bddd61
rjaduthie <rjaduthie@lenovo-debian>
2018-03-19 21:38:18 +0100

    Python query to get all temp sensors, lat/long, into csv
    Ordered west-east, the csv includes type, lat, long and URL ID
    
    Another CSV can have the data for these sensors, based on the URL ID

--- fiesta-client/source/fiesta-query-requests.py
+++ fiesta-client/source/fiesta-query-requests.py
@@ -38,9 +38,10 @@ WHERE {
 }
 '''
 
-sparql_query_order = '''  '''
-# ORDER BY ?sensingDevice ASC(?platform)
-# '''
+# order west to east
+sparql_query_order = ''' 
+ORDER BY ASC(?long)
+'''
 
 sparl_query_limit = '''
 LIMIT 100000
@@ -76,6 +77,7 @@ if 'items' not in query_results:
     sys.exit(1)
 
 n_sensors = len(query_results['items'])
+sensor_url = []
 sensor_type = []
 sensor_lat = []
 sensor_long = []
@@ -83,24 +85,33 @@ sensor_long = []
 # the next part parses the data - this could be made more generalised, but it doesn't need to be currently
 for result in query_results['items']:
 	
+    sensor_url_str = result['sensingDevice']
+    sensor_url.append(sensor_url_str)
+#     print('Attribute (url): %s' % result['sensingDevice'])
+    
     type_str = result['qk'].split('#')[-1]
-    print('Attribute (qk): %s' % result['qk'])
-    print('Parsed: %s' % type_str)
+#     print('Attribute (qk): %s' % result['qk'])
+#     print('Parsed: %s' % type_str)
     sensor_type.append(type_str)
     
     lat_str = result['lat'].split('^')[0]
     lat_val = float(lat_str)
-    print('Attribute (lat): %s' % result['lat'])
-    print('Parsed: %f' % lat_val)
+#     print('Attribute (lat): %s' % result['lat'])
+#     print('Parsed: %f' % lat_val)
     sensor_lat.append(lat_val)
     
     long_str = result['long'].split('^')[0]
     long_val = float(long_str)
-    print('Attribute (lat): %s' % result['long'])
-    print('Parsed: %f' % long_val)
+#     print('Attribute (lat): %s' % result['long'])
+#     print('Parsed: %f' % long_val)
     sensor_long.append(long_val)
     
 # now output these data to a CSV
-pass
+import itertools
+sensor_info = itertools.zip_longest(sensor_type,sensor_lat,sensor_long,sensor_url,fillvalue='NaN') # fillvalue compatible with javascript isNaN()
 	
-    
+import csv
+with open('data/sensors_lat_long.csv', 'w', newline='') as f:
+    writer = csv.writer(f)
+    writer.writerow(('sensor_type','lat','long','sensor_url'))
+    writer.writerows(sensor_info)
