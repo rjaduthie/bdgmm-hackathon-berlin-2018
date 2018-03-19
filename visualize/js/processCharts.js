575eeff200fd7eda1da473b996301a500ead8fc5
Tobias Weber <tobias.weber@lrz.de>
2018-03-19 14:26:35 +0100

    Status quo (timetable not working9

--- visualize/js/processCharts.js
+++ visualize/js/processCharts.js
@@ -48,7 +48,6 @@ loadJSON(function(response){
  * DATA PROCESSING => CREATE SOME CHARTS
  ******************************************************************************/
 d3.csv('data.csv', function (error, data) {
-    console.log(data)
     //Explicitely cast integers
     //data.forEach(function(datum) //{
         //datum[df.at] //= //+datum[df.at];
@@ -88,17 +87,14 @@ d3.csv('data.csv', function (error, data) {
      *************************************************************************/
     if (config.charts.Sensors === "true") {
         var sensorDimension = ndx.dimension(function (d) {
-            return d["Sensors"];
+            return d["Sensor"];
         });
         var sensors = sensorDimension.group().reduceCount();
-        dc.selectMenu('#Seonsors')
+        dc.selectMenu('#Sensors')
             .multiple(true)
-            .size(13)
+            .numberVisible(13)
             .dimension(sensorDimension)
             .group(sensors)
-            .title(function(d) {
-                return d.value 
-            })
             .order(function(a,b) {
                 return a.value < b.value ? 1 : b.value < a.value ? -1 : 0;
             })
@@ -110,76 +106,22 @@ d3.csv('data.csv', function (error, data) {
     /**************************************************************************
      * BARCHART OF COMMITS PER WEEKDAY (BOTTUM UP)
      *************************************************************************/
-    if (config.charts.authorWeekdayBar === "true") {
-        var weekdayLookup = [ "Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun" ];
-        var dayDimension = ndx.dimension(function (d) {
-            var date = new Date(d[df.at] *1000);
-            if(date.getDay() == 0 ) {
-                return weekdayLookup[6];
-            }
-            return weekdayLookup[date.getDay() -1];
-        });
-        var onWeekday = dayDimension.group().reduceCount();
-        dc.barChart('#authorWeekdayBar')
-            .width(400)
-            .height(250)
-            .elasticY(true)
-            .dimension(dayDimension)
-            .group(onWeekday)
-            .x(d3.scale.ordinal().range(weekdayLookup))
-            .xUnits(dc.units.ordinal)
-            .xAxisLabel("Weekday of author date")
-            .yAxisLabel("Number of Commits")
-            .ordering(function(d) {
-                return weekdayLookup.indexOf(d.key);
-            })
-            .on('renderlet', function(chart) {
-                chart.selectAll("svg").classed("center-block", true)
-            });
-    }
 
-    if (config.charts.committerWeekdayBar === "true") {
-        var weekdayLookup = [ "Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun" ];
-        var dayDimension = ndx.dimension(function (d) {
-            var date = new Date(d[df.ct] *1000);
-            if(date.getDay() == 0 ) {
-                return weekdayLookup[6];
-            }
-            return weekdayLookup[date.getDay() -1];
-        });
-        var onWeekday = dayDimension.group().reduceCount();
-        dc.barChart('#committerWeekdayBar')
-            .width(400)
-            .height(250)
-            .elasticY(true)
-            .dimension(dayDimension)
-            .group(onWeekday)
-            .x(d3.scale.ordinal().range(weekdayLookup))
-            .xUnits(dc.units.ordinal)
-            .xAxisLabel("Weekday of committer date")
-            .yAxisLabel("Number of Commits")
-            .ordering(function(d) {
-                return weekdayLookup.indexOf(d.key);
-            })
-            .on('renderlet', function(chart) {
-                chart.selectAll("svg").classed("center-block", true)
-            });
-    }
     /**************************************************************************
      * TIMELINE CHART (COMMITS PER DAY)
      *************************************************************************/
     if (config.charts.Datetime === "true") {
         var measurementByDatetime = ndx.dimension(function (d) {
             var exactDate = Date.parse(d["dateTime"])
-                console.log(d["dateTime"])
             return exactDate;
         });
+
 
         var internalTimeGroup = measurementByDatetime
             .group()
             .reduceCount(function(d) {
-                return d3.time.hour(new Date(d[df.Datetime] * 1000))
-            });
+                return Date.parse(d["dateTime"]);
+        });
 
 //        var patchesByCommittime = ndx.dimension(function (d) {
 //            var exactDate = new Date(d[df.ct] * 1000);
@@ -200,6 +142,7 @@ d3.csv('data.csv', function (error, data) {
             .group(internalTimeGroup);
 
 
+
 //        var commitLineChart = dc.lineChart(compositeTimeLineChart)
 //            .colors('green')
 //            .dimension(patchesByCommittime)
@@ -209,7 +152,6 @@ d3.csv('data.csv', function (error, data) {
         compositeTimeLineChart
             .width(600)
             .height(250)
-            .transitionDuration(50)
             .margins({top: 30, right: 50, bottom: 25, left: 40})
             .x(d3.time.scale()
                 .domain(d3.extent(data, function(d) {
@@ -238,7 +180,7 @@ d3.csv('data.csv', function (error, data) {
             .columns([
                 function (d) {return d["Sensor"];},
                 function (d) {return d["Observation"];},
-                function (d) {return Date.parse(d["dateTime"]);},
+                function (d) {return d["dateTime"];},
                 function (d) {return d["dataValue"];},
                 function (d) {return d["internal"];},
 
